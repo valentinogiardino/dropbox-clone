871cfd283efbdbf2b6b83c89126c70d56b29178e
Valentino Giardino <valentinogiardino17@gmail.com>
2023-12-31 17:20:46 -0300

    add table

---
+++ constants.ts
@@ -0,0 +1,27 @@
+export const COLOR_EXTENSION_MAP: { [KEY: string]: string} = {
+    pdf: "#0160FE",
+    docx: "#2b579a",
+    txt: "#000000",     // Black for text files
+    jpg: "#FFD700",     // Gold for JPEG images
+    jpeg: "#FFD700",    // Gold for JPEG images
+    png: "#00FFFF",     // Cyan for PNG images
+    gif: "#FF00FF",     // Magenta for GIF images
+    ppt: "#FF1493",     // DeepPink for PowerPoint files
+    pptx: "#FF1493",    // DeepPink for PowerPoint files
+    xls: "#00BFFF",     // DeepSkyBlue for Excel files
+    xlsx: "#00BFFF",    // DeepSkyBlue for Excel files
+    mpeg: "#FF4500",    // OrangeRed for MPEG audio/video
+    exe: "#FF0000",     // Red for executable files
+    js: "#F0DB4F",      // Gold for JavaScript files
+    css: "#008B8B",     // DarkCyan for CSS files
+    html: "#FFD700",    // Gold for HTML files
+    json: "#4682B4",    // SteelBlue for JSON files
+    xml: "#FFA500",     // Orange for XML files
+    sql: "#800080",     // Purple for SQL files
+    sip: "#8A2BE2",     // BlueViolet for SIP files
+    dmg: "#696969",     // DimGray for DMG files
+    psd: "#9932CC",     // DarkOrchid for PSD files
+    svg: "#008000",     // Green for SVG files
+    ai: "#FF4500"       // OrangeRed for AI files
+    // Add more file extensions and colors as needed
+}
