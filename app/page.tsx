deed84caa15182114fedad00db941525684b927f
Valentino Giardino <valentinogiardino17@gmail.com>
2023-12-31 13:58:46 -0300

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,44 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="">
-      <h1>Dropbox clone</h1>
+      <div className="flex flex-col lg:flex-row items-center bg-[#1E1919] dark:bg-slate-800">
+        <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5">
+          <h1 className="text-5xl font-bold">
+            Welcome to Dropbox
+            <br/>
+            <br/>
+            Storing everything for you and your business needs. All in one place
+          </h1>
+          
+          <p className="pb-20">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati quos sunt, veniam, error iure consequuntur esse ratione eius libero numquam vero dolorum pariatur et nam maiores repellendus eos quibusdam!
+          </p>
+
+          <Link href='/dashboard' className="flex cursor-pointer bg-blue-500 p-5 w-fit">
+            Try it for free!
+            <ArrowRight className="ml-10" />
+          </Link>
+        </div>
+
+        <div className="bg-[#1E1919] dark:bg-slate-800 h-full p-10" >
+          <video autoPlay loop muted className="rounded-lg">
+            <source 
+              src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+      </div>
+
+      <p className="text-center font-bold text-xl pt-5">Disclaimer</p>
+      <p className="text-center font-light p-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam iure aliquam officiis culpa. Libero facilis impedit sunt sequi rem? Rem aliquid magni vel labore voluptatem veritatis veniam nisi explicabo totam.</p>
 
 
-      <Button variant={'ghost'} >Button</Button>
     </main>
   )
 }
